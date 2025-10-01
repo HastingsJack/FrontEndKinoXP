2fde313634ec0e1a932cd88e19c848bce814f3d0
HastingsJack <joha0005@stud.kea.dk>
2025-10-01 14:07:45 +0200

    html og js for add movie form

---
+++ js/addmovieform.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("formMovie")
+const messageDiv = document.getElementById("message");
+
+function fetchMovie(event) {
+    event.preventDefault();
+    const movieID = document.getElementById("inpCode").value
+    fetch(`http://localhost:8080/movies/${movieID}`, {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'}
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Kunne ikke finde en film med dette id')
+        }
+        return response.json();
+    }).then(movie => {
+        messageDiv.textContent = `Filmen "${movie.title}" er gemt i databasen!`;
+        messageDiv.style.color = 'green';
+    })
+        .catch(error => {
+            messageDiv.textContent = error.message;
+            messageDiv.style.color = 'red';
+        });
+}
+
+form.addEventListener('submit', fetchMovie)
