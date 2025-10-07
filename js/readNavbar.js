f14fc995b93b50385190d436f559749457dcf709
mathi <mathiasepc@msn.com>
2025-10-07 11:39:32 +0200

    navbar added logout and addmovie

--- js/readNavbar.js
+++ js/readNavbar.js
@@ -1,25 +1,36 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const navbar = document.getElementById("navbarContainer");
-    const response = await (await fetch("./fragment/navbar.html")).text();
-
-    navbar.innerHTML = response;
+    navbar.innerHTML = await (await fetch("./fragment/navbar.html")).text();
 
     const snackButton = document.getElementById("snacks")
-    snackButton.addEventListener("click", e => {
+    snackButton.addEventListener("click", (e) => {
         e.preventDefault()
         location.href = "./get-snacks.html"
-    })
+    });
 
     const activeFilms = document.getElementById("activeFilms")
-    activeFilms.addEventListener("click", e => {
+    activeFilms.addEventListener("click", (e) => {
         e.preventDefault()
         location.href = "./active-films.html"
-    })
+    });
 
     const adminEmployeesButton = document.getElementById("adminEmployees")
-    adminEmployeesButton.addEventListener("click", e => {
+    adminEmployeesButton.addEventListener("click", (e) => {
         e.preventDefault()
         location.href = "./admin-view-users.html"
-    })
+    });
+
+    const adminMoviesButton = document.getElementById("addMovie")
+    adminMoviesButton.addEventListener("click", (e) => {
+        e.preventDefault()
+        location.href = "./add-movie.html"
+    });
+
+    const logoutButton = document.getElementById("logout")
+    logoutButton.addEventListener("click", (e) => {
+        e.preventDefault()
+        localStorage.clear()
+        location.href = "./index.html"
+    });
 
 })
